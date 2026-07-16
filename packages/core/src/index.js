094786a83b44f22586524bd74aa0b7dfbc7bec7b
Muhammad Waseem <linkme.waseem@gmail.com>
2026-07-16 14:44:25 +0500

    refactor: reorganize exports in index.js for improved structure and clarity

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,8 +1,43 @@
-export { default as Application } from "./Application.js";
+// ---- Container & Application ----
 export { default as Container } from "./Container.js";
+export { default as Application } from "./Application.js";
 export { default as ServiceProvider } from "./ServiceProvider.js";
-export { default as Facade } from "./Facade.js"
-export { default as ConfigManager } from "./ConfigManager.js";
-export { default as ConfigError } from "./errors/ConfigError.js";
-export { default as ContainerError } from "./errors/ContainerError.js";
+export { default as Facade } from "./Facade.js";
+
+// ---- Errors ----
 export { default as ECFError } from "./errors/ECFError.js";
+export { default as ContainerError } from "./errors/ContainerError.js";
+export { default as ConfigError } from "./errors/ConfigError.js";
+export { default as LoggerError } from "./errors/LoggerError.js";
+export { default as EventError } from "./errors/EventError.js";
+export { default as EnvError } from "./errors/EnvError.js";
+
+// ---- Config ----
+export { default as ConfigManager } from "./ConfigManager.js";
+export { default as ConfigServiceProvider } from "./providers/ConfigServiceProvider.js";
+export { default as Config } from "./facade/Config.js";
+
+// ---- Logger ----
+export { default as LoggerManager } from "./LoggerManager.js";
+export { default as LoggerServiceProvider } from "./providers/LoggerServiceProvider.js";
+export { default as Log } from "./facade/Log.js";
+export { default as Transport } from "./transports/Transport.js";
+export { default as ConsoleTransport } from "./transports/ConsoleTransport.js";
+
+// ---- Events ----
+export { default as EventManager } from "./events/EventManager.js";
+export { default as EventServiceProvider } from "./providers/EventServiceProvider.js";
+export { default as Event } from "./facade/Event.js";
+
+// ---- Environment ----
+export { default as EnvManager } from "./env/EnvManager.js";
+export { default as DotEnvLoader } from "./env/DotEnvLoader.js";
+export { default as EnvironmentServiceProvider } from "./providers/EnvironmentServiceProvider.js";
+export { default as Env } from "./facade/Env.js";
+
+// ---- Database ----
+export { default as DatabaseServiceProvider } from "./providers/DatabaseServiceProvider.js";
+
+// ---- NOT exported yet ----
+// facade/DB.js is an empty file — implement it, then add:
+// export { default as DB } from "./facade/DB.js";
